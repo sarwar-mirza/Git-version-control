a2a623966abdc0d1a9d3d1f8f859601c4e24cc07
mithu <nilchirkutpoison@gmail.com>
2024-03-20 14:38:07 +0600

    Create database

--- enroll/admin.py
+++ enroll/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ImageUpload
 
 # Register your models here.
+@admin.register(ImageUpload)
+class ImageUploadAdmin(admin.ModelAdmin):
+    # Ordering fields name
+    list_display = ['id', 'title', 'desc', 'picture', 'date']
+
+
+
--- enroll/models.py
+++ enroll/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ImageUpload(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    picture = models.ImageField(upload_to = 'myimage')
+    date = models.DateTimeField(auto_now_add=True)
+
+
