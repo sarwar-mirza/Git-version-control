c81f62ce5629a331ee0b56402646168c4025a7b0
mithu <nilchirkutpoison@gmail.com>
2024-03-20 16:03:01 +0600

    Business logic for home page

--- enroll/views.py
+++ enroll/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import ImageUpload
 
 # Create your views here.
+class HomeTemplateView(TemplateView):
+    template_name = 'enroll/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        img = ImageUpload.objects.all()                 # all data access in database
+        context = {'image': img}
+        return context
+    
