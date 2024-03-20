0a83c7b462cfd4155751f7c9c1765a8e146269e7
mithu <nilchirkutpoison@gmail.com>
2024-03-20 20:25:37 +0600

    Sign up Business logic

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .forms import SignUpUserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+# Create Signup views here.
+class SignUpTemplateView(TemplateView):
+    template_name = 'authentication/signup.html'
+    
+    def get_context_data(self, **kwargs):    # create get function
+        context = super().get_context_data(**kwargs)
+        fm = SignUpUserCreationForm()
+        context = {'form':fm}
+        return context
+    
+    
+    def post(self, request):     # create post function
+        fm = SignUpUserCreationForm(request.POST)
+        
+        if fm.is_valid():       # check valid form
+            fm.save()
+            
+            # messages framework
+            messages.success(request, 'Congratulations!!! your account has been created successfully')
+            fm = SignUpUserCreationForm()       # empty form
+        return render(request, 'authentication/signup.html', {'form':fm})
+
+
+
+
+
