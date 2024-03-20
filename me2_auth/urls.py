92387ba14c227eb1cc6cee1d7ff74ccd9ccbd4d6
mithu <nilchirkutpoison@gmail.com>
2024-03-20 20:30:56 +0600

    sign up all file

--- me2_auth/urls.py
+++ me2_auth/urls.py
@@ -24,5 +24,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('enroll.urls')),
+    path("accounts/", include('authentication.urls')),
     
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
